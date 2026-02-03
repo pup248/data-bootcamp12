b4daeb349f8a36e794f0e8a99020990208125f3e
pup248 <pp.buffthepup@gmail.com>
2026-02-03 17:23:22 +0700

    Add now I know

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 # hello world
-print("Hello World")
-
-# print goodbye
-print("Goodbye")
-
-# hello from github
-print("Hello from GitHub")
-
-# new function
-def add_two(num):
-  return num+2
-
-print(add_two(2), add_two(5))
+print("์Now I know, GitHub is easy")
